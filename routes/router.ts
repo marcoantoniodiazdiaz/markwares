9a60349b086344a6bc101087e249d6a0aa5aae8a
Marco Antonio Diaz Diaz <kingmarkdc@gmail.com>
2020-08-16 18:10:35 -0500

    Precios

--- routes/router.ts
+++ routes/router.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from 'express';
+import { json } from 'body-parser';
 
+const { exec } = require("child_process");
 const app = Router();
 
 export { app };
@@ -10,5 +12,28 @@ app.get('/status', (req: Request, res: Response) => {
         data: 'RUNNING'
     });
 });
+
+app.get('/process', (req: Request, res: Response) => {
+
+    exec("pm2 jlist", (error: any, stdout: any, stderr: any) => {
+        if (error) {
+            return res.status(400).json({
+                ok: true,
+                data: error.message
+            });
+        }
+        if (stderr) {
+            return res.status(400).json({
+                ok: true,
+                data: stderr.message
+            });
+        }
+        return res.json({
+            ok: true,
+            data: JSON.parse(stdout),
+        });
+    });
+});
+
 
 export default app;
